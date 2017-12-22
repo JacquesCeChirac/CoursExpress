814b2999279f6d2ec8c11c85fc6d3f1bc943a42d
JacquesCeChirac <nicolas.cherridi@gmail.com>
2017-12-22 11:29:07 +0100

    test

--- routes/flickr.js
+++ routes/flickr.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+    /*var array = ["guitar", "car", "dog", "cat", "pc", "house", "bike"];
+    var random = Math.floor(Math.random() * array.length);
+    var choice = array[random];
+    var recherche = "";
+    if (req.query['find']){
+        recherche = req.query['find'];
+        param = 1;
+        res.render('flickr', {param: param});
+        res.render('flickr', {recherche: recherche});
+    } else {
+        recherche = choice;
+        param = 0;
+    }*/
     var Flickr = require("flickrapi"),
         flickrOptions = {
             api_key: "d945e9ae57ea663a201b570936208986",
@@ -12,18 +24,16 @@ router.get('/', function(req, res, next) {
             access_token: "72157691291972295-efb304fd9b8cd5fa",
             access_token_secret: "ef5948297c22e92e"
         };
-
     Flickr.authenticate(flickrOptions, function(error, flickr) {
         flickr.photos.search({
-            text: "guitar"
+            text: recherche
         }, function(err, result) {
-            res.render('flickr', { resultats: result });
-            console.log(result);
+            resultats = result;
             if(err) { throw new Error(err); }
             // do something with result
         })
     });
-
+    res.render('flickr', {resultats: result});
 });
 
 module.exports = router;
